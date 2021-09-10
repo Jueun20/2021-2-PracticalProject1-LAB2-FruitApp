aa62d0958f4d66f077b69e6968e35356b4415a83
seojueun <seojueun@172.17.202.248>
2021-09-10 21:34:06 +0900

    Complete Fruit.java

--- src/com/practicalproject/lab2/fruit/Fruit.java
+++ src/com/practicalproject/lab2/fruit/Fruit.java
@@ -1,5 +1,27 @@
 package com.practicalproject.lab2.fruit;
 
-public class Fruit {
+import java.util.Comparator;
 
+public class Fruit {
+	int no;
+	String name;
+	int price;
+	
+	public Fruit (int num, String nm, int pr){
+		this.no = num;
+		this.name = nm;
+		this.price = pr;
+	}
+	
+	class FruitComparator implements Comparator<Fruit> {
+		public int compare(Fruit f1, Fruit f2){
+			return f1.name.compareTo(f2.name);
+		}
+	}
+	
+	class FruitComparatorDesc implements Comparator<Fruit> {
+		public int compare(Fruit f1, Fruit f2){
+			return f2.name.compareTo(f1.name);
+		}
+	}
 }
